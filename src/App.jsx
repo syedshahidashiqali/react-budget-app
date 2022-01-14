be550a52bb291b4f795091e2f83baed4def4e7ee
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-01-14 12:25:44 +0500

    Budget Card component completed wrt design
    renamed App.js extension to jsx and add Internation currency formatter in utils.js and some more stuff

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Container, Stack, Button } from "react-bootstrap";
+import { BudgetCard } from "./components/BudgetCard";
+
+function App() {
+  return (
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+      <div 
+        style={{
+          display:"grid",
+          gridTemplateColumns:"repeat(auto-fill, minmax(200px, 1fr))",
+          gap:"1rem",
+          alignItems:"flex-start",
+      }}>
+        <BudgetCard name="Entertainment" amount={200} max={1000}   />
+      </div>
+    </Container>
+  );
+}
+
+export default App;
