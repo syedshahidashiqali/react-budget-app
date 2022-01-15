c5c4a25cd6c816f59c141f0c2316b3a6d39f31f8
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-01-15 15:06:14 +0500

    Update BudgetContext.js
    add functionality for when i remove whole budget its expenses should transfer to uncategorized budget and project finished with it

--- src/context/BudgetContext.js
+++ src/context/BudgetContext.js
@@ -34,7 +34,12 @@ export const BudgetsProvider = ({ children }) => {
   };
 
   const deleteBudget = ({ id }) =>{
-    // TODO deal with expenses
+    setExpenses(prevExpenses => {
+      return prevExpenses.map(expense => {
+        if(expense.budgetId !== id) return expense;
+        return {...expense, budgetId: UNCATEGORIZED_BUDGET_ID};
+      });
+    });
     setBudgets(prevBudgets => {
       return prevBudgets.filter(budget => budget.id !== id);
     });
